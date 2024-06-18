c09fc9bdc9c1f485bf0156c822fbd55db52e2882
shiralog <shiralog2000@gmail.com>
2024-06-18 17:45:13 +0300

    Fix bugs in admin-reports

--- javascript/admin.js
+++ javascript/admin.js
@@ -3,7 +3,7 @@ document.addEventListener('DOMContentLoaded', function () {
     let username = prompt("Enter username:");
     let password = prompt("Enter password:");
 
-    while (username !== "admin" && password !== "admin") {
+    while (username !== "admin" || password !== "admin") {
         alert("Invalid username or password. Please try again.");
         username = prompt("Enter username:");
         password = prompt("Enter password:");
@@ -49,21 +49,27 @@ function fetchReports() {
         .then(reports => {
             const reportsTableBody = document.querySelector('#reportsTable tbody');
             reportsTableBody.innerHTML = ''; // Clear existing rows
-
-            reports.forEach(report => {
-                const row = document.createElement('tr');
-                row.innerHTML = `
-                    <td>${report.IssueTopic}</td>
-                    <td>${report.IssueContent}</td>
-                    <td>${report.TicketID}</td>
-                    <td>${report.DateOfSubmission}</td>
-                    <td>
-                        <input type="text" placeholder="Enter response" id="response_${report.TicketID}">
-                        <button onclick="submitResponse(${report.TicketID})">Submit</button>
-                    </td>
-                `;
-                reportsTableBody.appendChild(row);
-            });
+            console.log(reports.length);
+            if (reports.length > 0) {
+                document.getElementById('reportsTable').style.display = 'block';
+                reports.forEach(report => {
+                    const row = document.createElement('tr');
+                    row.innerHTML = `
+                        <td>${report.IssueTopic}</td>
+                        <td>${report.IssueContent}</td>
+                        <td>${report.TicketID}</td>
+                        <td>${report.DateOfSubmission}</td>
+                        <td>
+                            <input type="text" placeholder="Enter response" id="response_${report.TicketID}">
+                            <button onclick="submitResponse(${report.TicketID})">Submit</button>
+                        </td>
+                    `;
+                    reportsTableBody.appendChild(row);
+                });
+            } else {
+                document.getElementById('reportsTable').style.display = 'none';
+                document.getElementById('status').innerHTML += `No reports`;
+            }
         })
         .catch(error => console.error('Error fetching reports:', error));
 }
